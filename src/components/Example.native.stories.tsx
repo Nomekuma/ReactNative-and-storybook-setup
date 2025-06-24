b8d129e590ecca4055a83e38063a08bef6840ae2
Nomekuma <t.gunness35@gmail.com>
2025-06-24 17:09:28 +0400

    feat: update Home component layout and enhance Storybook story definition

--- src/components/Example.native.stories.tsx
+++ src/components/Example.native.stories.tsx
@@ -1,10 +1,11 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { Example } from './Example';
+import type { Meta, StoryObj } from "@storybook/react";
+import { Example } from "./Example";
 
 const meta: Meta<typeof Example> = {
   component: Example,
+  title: "Atoms/Example",
 };
 export default meta;
 type Story = StoryObj<typeof Example>;
 
-export const Default: Story = {};
+export const Default: Story = () => <Example />;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,21 @@
-import { Text } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export const Home = () => {
-  return <Text>Home</Text>;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Welcome to the Home Page!</Text>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 20,
+    color: "#333",
+  },
+});
